5756d5f9722c232ded90c46283aaa4f799f7e564
Paweł Staniul <pstaniul@gmail.com>
2023-08-24 09:48:35 +0200

    Uzupełniono plik 'Program.cs' o niezbędne elementy konfiguracji

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,25 +1,98 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Globalization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MiodOdStaniula.Models;
+using MiodOdStaniula.Services;
+using MiodOdStaniula.Services.Interfaces;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace MiodOdStaniula
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromDays(2);
+            });
+
+            builder.Services.AddScoped<IAddProductService, AddProductService>();
+            builder.Services.AddScoped<ICartService, CartService>();
+            builder.Services.AddScoped<ICheckoutService, CheckoutService>();
+            builder.Services.AddScoped<ICustomerService, CustomerService>();
+            builder.Services.AddScoped<IDeleteService, DeleteService>();
+            builder.Services.AddScoped<IEditProductService, EditProductService>();
+            builder.Services.AddScoped<IFileUploadService, FileUploadService>();
+            builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddScoped<ITotalCostService, TotalCostService>();
+            builder.Services.AddScoped<IWarehouseService, WarehouseService>();
+
+            builder.Services.AddDbContext<DbStoreContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("miodOdStaniula"));
+            });
+
+
+            builder.Services.AddIdentity<UserModel, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+
+            }).AddEntityFrameworkStores<DbStoreContext>();
+
+            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[] { new CultureInfo("pl-PL") };
+                options.DefaultRequestCulture = new RequestCulture("pl-PL", "pl-PL");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.Strict,
+            });
+
+            var locOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+            if (locOptions != null)
+            {
+                app.UseRequestLocalization(locOptions.Value);
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseAuthorization();
+            app.UseAuthentication();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();

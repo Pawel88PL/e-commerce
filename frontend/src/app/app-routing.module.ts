6c37b566e63fa50ca205b69c051599d5fec1499e
Paweł Staniul <pstaniul@gmail.com>
2023-08-29 11:35:23 +0200

    Dodano komponent 'product-add.component'

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { WarehouseComponent } from './components/warehouse/warehouse.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'addproduct', component: ProductAddComponent },
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,28 @@
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { WarehouseComponent } from './components/warehouse/warehouse.component';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatSelectModule } from '@angular/material/select';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { WarehouseComponent } from './components/warehouse/warehouse.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+
 
 @NgModule({
   declarations: [
@@ -29,7 +33,8 @@ import { WarehouseComponent } from './components/warehouse/warehouse.component';
     ProductDetailsComponent,
     FooterComponent,
     NavbarComponent,
-    WarehouseComponent
+    WarehouseComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
@@ -40,6 +45,7 @@ import { WarehouseComponent } from './components/warehouse/warehouse.component';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
+    MatMenuModule,
     MatToolbarModule,
     MatSelectModule,
     SlickCarouselModule

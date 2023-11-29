13ea463d987bf481caac8348b0ce2a311314915a
Paweł Staniul <pstaniul@gmail.com>
2023-11-29 09:17:42 +0100

    Rozszerzono klasę IdentityUser o dodatkowe pola, przeprowadzono migrację i update bazy danych

--- backend/Models/UserModel.cs
+++ backend/Models/UserModel.cs
@@ -1,8 +1,32 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace MiodOdStaniula.Models
 {
     public class UserModel: IdentityUser
     {
+        [Required(ErrorMessage = "Wpisz swoje imię")]
+        [StringLength(50, ErrorMessage = "Imię jest za długie.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Wpisz swoje nazwisko")]
+        [StringLength(50, ErrorMessage = "Nazwisko jest za długie.")]
+        public string Surname { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Wpisz miejscowość")]
+        [StringLength(50, ErrorMessage = "Nazwa miejscowości jest za długa.")]
+        public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Wpisz ulicę")]
+        [StringLength(50, ErrorMessage = "Nazwa ulicy jest za długa.")]
+        public string Street { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Wpisz nr domu")]
+        [StringLength(20, ErrorMessage = "Numer domu jest za długi.")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Podaj kod pocztowy")]
+        [RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Niepoprawny format kodu pocztowego.")]
+        public string PostalCode { get; set; } = string.Empty;
     }
 }
